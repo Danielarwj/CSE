17752465e70231b103faa270117283c71a874745
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-10-24 09:45:53 -0700

    Added our first notes

--- notes/Hello World.py
+++ notes/Hello World.py
@@ -1,14 +1,57 @@
-print("Hello World!")
+print("Hello World")
+
+# This is a comment. This has no effect on the code
+# but this does allow me to do things. I can:
+# 1. Make notes to myself
+# 2. Comment pieces of code that do not work
+# 3. Make my code easier to read
+
+print("Look at what happens here. Is there any space")
+print()
+print()
+print("There should be a couple blank lines here")
+
+#Math
+print(3+5)
+print(5-2)
+print(3*4)
+print(6/2)
 
 
 
+print("Here is another one...")
+print(6%2)
+print (5 % 2)
+print(11%4) # Modulus (Remainder)
+
+#Powers
+#What is 2^20?
+
+print(2**100)
+print(3**27)
+name=input("What is your name")
+print("Hello %s. " % name)
+
+age= input("How old are you? >_")
+print("%s?!? You Belong in a Museum" % age)
+print()
+print("%s is really old. They are %s years old " % (name,age))
 
 
+#variable assignments
+car_name=" Weibe Mobile"
+car_type="Tesla"
+car_cylinders="16"
+car_miles_per_gallon="0.01"
 
+#Make it print"I have a car called the Weibe Mobile. It is a Tesla
 
+print("I have a car called %s. It is a %s" % (car_name, car_type))
 
+print("My car has %s cylinders and it has %s miles per gallon" % (car_cylinders, car_miles_per_gallon))
 
-
-
-
+# Recasting
+real_age=int(input("How old are you again?"))
+hidden_age=real_age+5
+print("This is your real age: %d" % hidden_age)
 
