e78b56ff425ae22a19712bf5abf895cf957b3e7b
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-11-16 09:44:44 -0800

    Finished notes on tuples,sets, and dictionaries. Began Hangman the Game

--- notes/Daniel Remington-Tuple,Set,and Dictionary Notes.py
+++ notes/Daniel Remington-Tuple,Set,and Dictionary Notes.py
@@ -1,12 +1,25 @@
 tuple1 = ("apple", "banana", "cherry")
-print(tuple1)
 
-print(tuple1[1])
+this_set = {"pie", "cake", "cookie"}
+print(this_set)
 
-tuple1
+for x in this_set:
+    print(x)
+print("pie" in this_set)
 
+this_set.add("orange")
+print(this_set)
 
+this_set.update(["mango", "pineapple", "grapefruit"])
+print(this_set)
 
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(this_dict)
 
-
+x = this_dict.get("model")
+print(x)
 
--- notes/Hangman.py
+++ notes/Hangman.py
@@ -1,1 +1,5 @@
-
+import random
+words = ["car", "lawn", "drive", "plane", "train", "folder", "box", "eight", "book", "value"]
+word = random.choice(words)
+correct = word
+print(word)
