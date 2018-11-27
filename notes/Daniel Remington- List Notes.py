95fb91923486e2ab0657f72200cdfffceeb6b52b
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-11-27 09:48:20 -0800

    hangman notes

--- notes/Daniel Remington- List Notes.py
+++ notes/Daniel Remington- List Notes.py
@@ -72,43 +72,68 @@ print(Star_Trek_Characters[0:4])
 print(Star_Trek_Characters[1:4])
 
 
+food_list = ["Turkey", "Sweet Potato Casserole", "Eggs", "Squash", "Carrot", "Chocolate Cake", "Bacon",
+             "Chocolate covered Bacon","Salad",
+         "Bacon with extra fat", "chicken", "Fourth of July Cookies"," Peanut Butter and Chocolate Cookies",
+            "Apple Pie",
+            "Roasted Salmon", "Sushi", "Poke", "Noodles", "Saltine Crackers", "Celery", "Eggplant Parm","Enchilada"]
+print(len(food_list))
+
+food_list.append("Bacon")
+food_list.append("Eggs")
+
+# Notice that everything is object.method(parameters)
+print(food_list)
+
+food_list.insert(1, "eggo waffles")
+print(food_list)
+
+food_list.remove("Salad")
+print(food_list)
 
 
+"""
+1.Make a new list with three items
+2.Add fourth item
+3. Remove one of first three items
+"""
+
+Star_Wars_Characters = ["Luke", "Han", "Chewie"]
+
+Star_Wars_Characters.insert(3, "Darth Vader")
+print(Star_Wars_Characters)
+Star_Wars_Characters.remove("Luke")
+print(Star_Wars_Characters)
 
 
+# Tuples
+brands = ("apple", "samsung", "HTC") # Notice the ()'s
+print(food_list)
+food_list.pop(0)
+print(food_list)
 
 
+print(food_list.index("chicken"))
 
+string1 = "TURQUOISE"
+list1 = list(string1)
+print(list1)
 
+print("".join(list1))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(len(list1)):  # i goes through all indices
+    if list1[i] == "u":  # if we find a U
+        list1.pop(i)  # remove the i-th index
+        list1.insert(i, "*")  # put a * there instead
+     
+"""
+for character in list1:
+    if character == "u:
+        # replace with a*
+        current index = list1.index(character)
+        list1.pop(current_index)
+        list1.insert(current_index, "*"
+"""
 
 
 
