079e76dc26be342c66cf7297f0b4034eb4e9eacd
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-11-29 09:46:29 -0800

    Started hangman

--- notes/Hangman.py
+++ notes/Hangman.py
@@ -1,10 +1,39 @@
 import random
 words = ["car", "lawn", "drive", "plane", "train", "folder", "box", "eight", "book", "value"]
-word = random.choice(words)
-correct = word
-print(word)
+correct_word = random.choice(words)
+correct = correct_word
+print(correct_word)
 
+string_car = "car"
+string_lawn = "lawn"
+string_drive = "drive"
+string_plane = "plane"
+string_train = "train"
+string_folder = "folder"
+string_box = "box"
+string_eight = "eight"
+string_book = "book"
+string_value = "value"
 
+list_car = list(string_car)
+print(list_car)
 
+list_lawn = list(string_lawn)
+list_drive = list(string_drive)
+list_plane = list(string_plane)
+list_train = list(string_train)
+list_folder = list(string_folder)
+list_box = list(string_box)
+list_eight = list(string_eight)
+list_book = list(string_book)
+list_value = list(string_value)
 
-
+print(list_lawn)
+print(list_book)
+print(list_box)
+print(list_drive)
+print(list_eight)
+print(list_folder)
+print(list_plane)
+print(list_train)
+print(list_value)
