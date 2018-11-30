151b92cbd13d4670507eae621859be0d4f9c7306
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-11-30 13:12:43 -0800

    Actually started Hangman
    Use this point

--- notes/Hangman.py
+++ notes/Hangman.py
@@ -1,39 +1,11 @@
 import random
 words = ["car", "lawn", "drive", "plane", "train", "folder", "box", "eight", "book", "value"]
-correct_word = random.choice(words)
-correct = correct_word
-print(correct_word)
+secret_word = random.choice(words)
+disp_list = ["*" * len(secret_word)]
+letter_list = [list(secret_word)]
+guesses_left = 5
 
-string_car = "car"
-string_lawn = "lawn"
-string_drive = "drive"
-string_plane = "plane"
-string_train = "train"
-string_folder = "folder"
-string_box = "box"
-string_eight = "eight"
-string_book = "book"
-string_value = "value"
-
-list_car = list(string_car)
-print(list_car)
-
-list_lawn = list(string_lawn)
-list_drive = list(string_drive)
-list_plane = list(string_plane)
-list_train = list(string_train)
-list_folder = list(string_folder)
-list_box = list(string_box)
-list_eight = list(string_eight)
-list_book = list(string_book)
-list_value = list(string_value)
-
-print(list_lawn)
-print(list_book)
-print(list_box)
-print(list_drive)
-print(list_eight)
-print(list_folder)
-print(list_plane)
-print(list_train)
-print(list_value)
+while guesses_left > 0 and "*"in disp_list:
+    print(''.join(disp_list))
+    print("You have %d guesses left" % guesses_left)
+    guess = input("Guess a letter")
