4e57b6e7b5345918fb8bd118b0fc28fb76e5a498
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-12-04 13:38:41 -0800

    finished challenges

--- notes/Daniel Remington- Hard Challenges.py
+++ notes/Daniel Remington- Hard Challenges.py
@@ -1,4 +1,5 @@
-# challenge two is the datetime
+import math
+# challenge 10 is the datetime
 import datetime
 datetime.datetime.now()
 datetime.datetime(2018, 12, 4, 9, 17, 20)
@@ -7,6 +8,8 @@ print(datetime.datetime.now())
 vowels = ["a", "e", "i", "o", "u"]
 consonants = ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
 y_potential_vowel = "y"
+
+# Challenge 9
 
 
 def challenge_one(letter):
@@ -20,12 +23,25 @@ def challenge_one(letter):
 
 challenge_one("r")
 
+# Challenge 11
+
 
 def challenge_three(string):
-    if string == int:
-        print("Your string,%d, is an integer" % string)
-    else:
-        print("Your string,%s, is not an integer" % string)
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
 
 
-challenge_three(200)
+print(challenge_three(5))
+
+
+# Challenge 12
+
+
+def challenge_four(number_1, number_2):
+    return math.gcd(number_1, number_2)
+
+
+print(challenge_four(4, 8))
