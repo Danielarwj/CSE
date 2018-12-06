b71435986631f32b9219251bc630670e37b9549d
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-12-06 09:45:57 -0800

    continued extra challenges

--- notes/Extra Challenges.py
+++ notes/Extra Challenges.py
@@ -1,10 +1,24 @@
-
+"""
 number_list = range(2000, 3200)
 
 for i in number_list:
     if i % 7 == 0 and i % 5 != 0:
         print(i)
 
+"""
+
+letter_length = input("Insert a word")
+LETTERS = list(letter_length)
+print("The word is %s letters long" % len(LETTERS))
+
+number_length = input("Insert a number")
+NUMBERS = list(number_length)
+print("The number is %s digits long" % len(NUMBERS))
+
+
+def challenge_1(numbers):
+    list(numbers)
+    tuple(numbers)
 
 
 
