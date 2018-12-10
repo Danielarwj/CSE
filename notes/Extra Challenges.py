da4c1678ad854bc53c8d43e3acb5198ae842fdbc
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2018-12-10 09:44:48 -0800

    worked on random

--- notes/Extra Challenges.py
+++ notes/Extra Challenges.py
@@ -7,6 +7,7 @@ for i in number_list:
 
 """
 
+"""
 letter_length = input("Insert a word")
 LETTERS = list(letter_length)
 print("The word is %s letters long" % len(LETTERS))
@@ -15,14 +16,16 @@ number_length = input("Insert a number")
 NUMBERS = list(number_length)
 print("The number is %s digits long" % len(NUMBERS))
 
+"""
+
 
 def challenge_1():
     list()
     tuple()
 
-print(challenge_1(17,12))
 
+for i in range(5):
+    print(i)
 
-
-
-
+_ = "gh"
+print(_)
