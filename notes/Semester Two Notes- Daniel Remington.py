974e4afdcc75253e1d88088b2e8cbf34e332717f
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-01-08 09:44:50 -0800

    Started Semester Two Notes

---
+++ notes/Semester Two Notes- Daniel Remington.py
@@ -0,0 +1,28 @@
+print("Hello World")
+
+# This is a single line comment
+
+cars = 5
+
+driving = True
+
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars")
+
+age = input("How old are you")
+print("How my gosh! You are %s years old. I'm not even that old!" % age)
+
+colors = ["Red", "Blue", "Green", "Yellow", "Purple"]
+colors.append("Cyan")
+
+print(colors)
+
+# .Pop removes indexes and you don't know what you are deleting
+# .remove removes objects and you know what you are deleting with .remove
+
+colors.pop(0)
+print(colors)
+
+print(colors[2])
+
+print(len(colors))
