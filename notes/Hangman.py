e622a5fe22fbac5c77250a628e922555f458eff9
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-01-16 09:45:08 -0800

    Added Win Condition

--- notes/Hangman.py
+++ notes/Hangman.py
@@ -1,4 +1,5 @@
 import random
+import string
 words = ["car", "lawn", "drive", "plane", "train", "folder", "box", "eight", "book", "value",
          "Are you going to Scarborough fair?"]
 secret_word = random.choice(words)
@@ -8,21 +9,42 @@ guesses_left = 8
 guessed_letters = []
 # print(disp_list)
 guess = ' '
+win = False
+HIDDEN_CHAR = "_ "
 
-while guesses_left > 0 and "*" in disp_list:
+while guesses_left >= 0 and not win:
     # Show/Hide Letters
     output = []
     for i in range(len(secret_word)):
         if letter_list[i] in guessed_letters:
             output.append(letter_list[i])
         else:
-            output.append("_ ")
+            output.append(HIDDEN_CHAR)
     print("".join(output))
+
+    if guesses_left == 0 and HIDDEN_CHAR in output:
+        print("You are out of guesses and still haven't guessed the word yet. Shameful. GAME OVER.")
+        print("The word was: %s" % secret_word)
+        quit(0)
+
+    if " " in letter_list:
+        " ".replace(" ", "")
+
+    # Check Win Condition
+    if HIDDEN_CHAR not in output:
+        print("YOU WIN!!!!!!!!!!")
+        print("(Jerk.)")
+        win = True
+        continue
 
     print("You have %d guesses left" % guesses_left)
 
     # Take in a guess
     guess = input("Guess a letter")
+
+    if guess == " ":
+        print("That is not valid. Also, Space, Really?")
+
 
     # Add guess to list
     guessed_letters.append(guess)
@@ -33,5 +55,3 @@ while guesses_left > 0 and "*" in disp_list:
     else:
         print("Oops. That's wrong")
         guesses_left -= 1
-    if guesses_left == 0 and "*" in disp_list:
-        print("You are out of guesses and still haven't guessed the word yet. Shameful. GAME OVER.")
