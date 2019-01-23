a2d9fa410a2e3e5a05254f84c9358cc60c116f8f
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-01-23 09:44:49 -0800

    CAPITALIZATION AND PUNCTUATION

--- notes/Hangman.py
+++ notes/Hangman.py
@@ -1,12 +1,13 @@
 import string
 import random
-words = ["car", "lawn", "drive", "plane", "train", "folder", "box", "eight", "book", "value",
-         "Are you going to Scarborough fair?"]
-secret_word = "DOG"
+words = ["BEAR! RUN NOW!!", "lawn", "drive", "plane", "train", "folder", "box", "eight", "book", "value",
+         "Are you going to Scarborough fair?", "We are the champions", "Hallelujah Money", "Clint Eastwood",
+         "dogs"]
+secret_word = random.choice(words)
 display_list = list("*" * len(secret_word))
 letter_list = list(secret_word)
 guesses_left = 8
-guessed_letters = []
+guessed_letters = list(string.punctuation + " ")
 legal_letters = [string.ascii_uppercase, string.ascii_lowercase]
 # print(display_list)
 guess = ' '
@@ -18,7 +19,7 @@ while guesses_left > 0 and not win:
     # Show/Hide Letters
     output = []
     for i in range(len(secret_word)):
-        if letter_list[i] in guessed_letters:
+        if letter_list[i].lower() in guessed_letters:
             output.append(letter_list[i])
         else:
             output.append("_ ")
@@ -27,7 +28,7 @@ while guesses_left > 0 and not win:
     print("You have %d guesses left" % guesses_left)
 
     # Take in a guess
-    guess = input("Guess a letter")
+    guess = input("Guess a letter").lower()
 
     if guess == " ":
         print("That is not valid. Also, Space, Really?")
@@ -42,4 +43,11 @@ while guesses_left > 0 and not win:
     else:
         print("Oops. That's wrong")
         guesses_left -= 1
+    if guesses_left == 0 and not win:
+        print("You have lost! Shameful.")
+        print("The word was %s" % secret_word)
 
+    if "_" not in output and win is True:
+        guesses_left = 0
+        print("You have defeated me")
+        print("(jerk)")
