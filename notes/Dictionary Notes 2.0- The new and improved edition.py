401a75878df632aa8e3eb1d8b8850cba12a66130
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-01-29 09:45:41 -0800

    continued on dictionary notes 2.0

--- notes/Dictionary Notes 2.0- The new and improved edition.py
+++ notes/Dictionary Notes 2.0- The new and improved edition.py
@@ -9,6 +9,78 @@ released = {
 
 print(released)
 
+states = {
+    "CA": "California",
+    "FL": "Florida",
+    "AK": "Alaska",
+    "GA": "Georgia"
+}
 
+print(states["CA"])
+print(states["AK"])
 
+nested_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 39500000
+    },
+    "FL": {
+        "NAME": "Florida",
+        "POPULATION": 21300000
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 10500000
+    }
+}
 
+print(nested_dictionary["GA"]["POPULATION"])
+print(nested_dictionary["FL"]["NAME"])
+
+georgia = nested_dictionary["GA"]
+print(georgia)
+
+complex_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 39500000,
+        "CITIES": [
+            "Fresno",
+            "San Francisco",
+            "Los Angeles"
+        ]
+    },
+    "FL": {
+        "NAME": "Florida",
+        "POPULATION": 21300000,
+        "CITIES": [
+            "Miami",
+            "Orlando",
+            "Tampas",
+            "Jacksonville"
+        ]
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000,
+        "CITIES": [
+            "Anchorage",
+            "Fairbanks",
+            "Barrow"
+        ]
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 10500000,
+        "CITIES": [
+            "Atlanta",
+            "Savannah",
+            "Columbia"
+        ]
+    }
+}
+print(complex_dictionary["AK"]["CITIES"][0])
--- notes/OOP Notes.py
+++ notes/OOP Notes.py
@@ -55,4 +55,4 @@ def dance(self):
 # instantiate the object
 blu = Parrot("Blu", 10)
 
-# 
+#
