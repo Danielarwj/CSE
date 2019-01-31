c437924630163c8bafd8a0b21662aa7c88d25661
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-01-31 09:44:55 -0800

    finished directions and started associating places and paths with the directions mentioned in the list

--- notes/Daniel Remington- Dictionary World Map.py
+++ notes/Daniel Remington- Dictionary World Map.py
@@ -4,7 +4,7 @@ world_map = {
         'DESCRIPTION': "This is the classroom you are in right now. "
                        "There are two doors on the north wall",
         'PATHS': {
-            'NORTH': 'PARKING LOT'
+            'NORTH': 'PARKING_LOT'
 
         }
     },
@@ -27,6 +27,7 @@ world_map = {
 # Controller
 playing = True
 current_node = world_map['R19A']
+directions = ['NORTH', 'SOUTH', 'NORTHEAST', 'SOUTHEAST', 'SOUTHWEST', 'NORTHWEST', "WEST", "EAST", "UP", "DOWN"]
 while playing:
     print(current_node["NAME"])
     print(current_node["DESCRIPTION"])
@@ -34,6 +35,11 @@ while playing:
     if command.lower() in ["q", "quit", 'exit']:
         playing = False
         print("GAME OVER.")
-
-
-
+    elif command.upper() in directions:
+        try:
+            room_name = current_node["PATHS"][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way!")
+    else:
+        print("Command not found")
