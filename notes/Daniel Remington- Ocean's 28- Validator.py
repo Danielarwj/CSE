bb7291a68f70ea9002b9454b2c4f617a12cdeab7
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-05-02 09:44:43 -0700

    Ocean's 28 is in full motion- (The csv file completly works)

--- notes/Daniel Remington- Ocean's 28- Validator.py
+++ notes/Daniel Remington- Ocean's 28- Validator.py
@@ -1,11 +1,11 @@
-import math
+# import math
+
 import csv
 
 
 def validate_card_number(num: str):
     if not sixteen_digits(num):
         return False
-    first_num = int(num[0])
     last_num = int(num[15])
 
     list_num = list(num)
@@ -19,8 +19,11 @@ def validate_card_number(num: str):
                 list_num[index] -= 9
     list_num_sum = sum(list_num)
     if list_num_sum % 10 == last_num:
-        print("This is a real number! We're in, boys!")
-
+        print("This is a real number! We're in, boys!: %d" % int(num))
+        return True
+    else:
+        print("This is a false number; %d" % int(num))
+        return False
 
 
 # list_num = list(num)
@@ -40,6 +43,21 @@ validate_card_number(str(1707909249311050))
 
 with open("Book1.csv", "r") as old_csv:
     with open("MyNewFile.csv", "w", newline='') as new_csv:
+        print("Writing file .......")
+        writer = csv.writer(new_csv)
+        reader = csv.reader(old_csv)
+        for row in reader:
+            old_number = row[0]
+
+            if not validate_card_number(old_number):
+                writer.writerow(row)
+            # print(int(old_number) +1)
+            # print(old_number)
+
+print("OK")
+
+with open("Book1.csv", "r") as old_csv:
+    with open("We're_in_with_these_ones_boys.csv", "w", newline='') as new_csv:
         print("Writing file .......")
         writer = csv.writer(new_csv)
         reader = csv.reader(old_csv)
