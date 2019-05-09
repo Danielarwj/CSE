0858a136362186f9a9d169b9aa30efab5180f500
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-05-09 09:47:17 -0700

    This... is the last github- (It works just fix the file)

--- notes/Daniel Remington- Ocean's 28- Github Breaker.py
+++ notes/Daniel Remington- Ocean's 28- Github Breaker.py
@@ -1,10 +1,13 @@
 import csv
 
-def OfficeSupplies(aspect):
+
+def officesupplies(aspect):
     if aspect == "Office Supplies":
         print(aspect)
 
 
+def total_profit(num):
+    profit_list.append(num)
 
 with open("SalesInformation.csv", "r") as old_csv:
     with open("SalesFile.csv", "w", newline='') as new_csv:
@@ -23,21 +26,42 @@ with open("SalesInformation.csv", "r") as old_csv:
         personal_list = []
         clothes_list = []
         fruits_list = []
-        types_of_items = {
-        }
+        profit_list = []
 
         for row in reader:
             old_number = row[2]
+            profit = float(row[13])
             print(old_number)
             if old_number == "Office Supplies":
                 office_list.append(old_number)
-            # if old_number == "Fruits":
-            #     old_number.append(fruits_list)
-            # if old_number == "Beverages":
-            #     old_number.append(beverages_list)
-            # if old_number == "Personal Care":
-            #     old_number.append(personal_list)
+                profit_list.append(total_profit(profit))
+            if old_number == "Cosmetics":
+                cosmetics_list.append(old_number)
+            if old_number == "Baby Food":
+                baby_food_list.append(old_number)
+            if old_number == "Beverages":
+                beverages_list.append(old_number)
+            if old_number == "Cereal":
+                cereal_list.append(old_number)
+            if old_number == "Household":
+                household_list.append(old_number)
+            if old_number == "Vegetables":
+                vegetables_list.append(old_number)
+            if old_number == "Meat":
+                meat_list.append(old_number)
+            if old_number == "Fruits":
+                fruits_list.append(old_number)
+            if old_number == "Clothes":
+                clothes_list.append(old_number)
+            if old_number == "Snacks":
+                snacks_list.append(old_number)
+            if old_number == "Personal Care":
+                personal_list.append(old_number)
 
-print(office_list)
 
+print(len(office_list))
+print(len(clothes_list))
+print(len(fruits_list))
+print(len(personal_list))
 
+print(sum(profit_list))
