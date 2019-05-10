94fe0191ab6177aed9e104baa54ade238ff0b3d8
Danielarwj <44374130+Danielarwj@users.noreply.github.com>
2019-05-10 10:41:53 -0700

    IT WORKS

--- notes/Daniel Remington- Ocean's  28- The Last Github.py
+++ notes/Daniel Remington- Ocean's  28- The Last Github.py
@@ -2,19 +2,40 @@ import csv
 
 items = {}
 
+averages = {}
 
-with open("SalesInformation.csv", "r") as old_csv:
+with open("Sales Records.csv", "r") as old_csv:
+    print(" ")
+    print("Mutants and humans. They have long struggled to coexist. While some try to unite the world, others try to "
+          "dominate it.")
+    print(" ")
+    print("Neither strategy has prevailed. But when conflicts reach an impasse, inevitably something "
+          "happens to shift the balance forever.")
+
     reader = csv.reader(old_csv)
     for row in reader:
         if row[0] == 'Region':
             continue
         item_type = row[2]
         profit = float(row[13])
+        units_sold = float(row[8])
+        unit_price = row[9]
+        unit_cost = row[10]
+
         try:
             items[item_type] += profit
         except KeyError:
             items[item_type] = profit
 
+        for item in items.items():
+            try:
+                averages[item_type] = profit / units_sold
+            except KeyError:
+                averages[item_type] = profit
+
+
+
 for key, item in items.items():
     print(key, end=": ")
     print("${:,}".format(round(item, 2)))
+    print(averages)
